b7f93a348d2fef619e7ada925c43c1a6bb294f9a
Adam Gulczyński <a.a.gulczynski@gmail.com>
2023-10-26 15:18:04 +0200

    update project card to reflect backend structural changes (#26)

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,8 @@ import { registerPlugins } from '@/plugins'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGithub, faArrowRight);
+library.add(faGithub);
 
 const app = createApp(App)
 
